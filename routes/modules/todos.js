41613b8a4050997c1bcdecd0bb21cf3d2b51bf79
weizi0328 <ankewang26@gmail.com>
2022-09-19 18:17:35 +0800

    add delete todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -54,10 +54,11 @@ router.put('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// 
 router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+
+  return Todo.findOne({ where: { id, UserId } })
     .then(todo => todo.destroy())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
